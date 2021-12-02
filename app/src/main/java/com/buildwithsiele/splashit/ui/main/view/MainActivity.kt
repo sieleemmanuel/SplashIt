9edbd35274899d4bf0c504c5740b9f1f66f56cdc
kimsiele <sielekim.emmanuel@gmail.com>
2021-12-02 19:46:35 +0300

    navigation UI update

--- app/src/main/java/com/buildwithsiele/splashit/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/buildwithsiele/splashit/ui/main/view/MainActivity.kt
@@ -2,12 +2,28 @@ package com.buildwithsiele.splashit.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.buildwithsiele.splashit.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_SplashIt)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
