41354b264bc59121711d52e99c3fee9dc7a195e3
kimsiele <sielekim.emmanuel@gmail.com>
2021-12-22 10:54:27 +0300

    Fixes

--- app/src/main/java/com/buildwithsiele/splashit/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/buildwithsiele/splashit/adapters/ViewPagerAdapter.kt
@@ -5,18 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.buildwithsiele.splashit.R
 import com.buildwithsiele.splashit.data.model.Photo
 import com.squareup.picasso.Picasso
 
-class ViewPagerAdapter:RecyclerView.Adapter<ViewPagerAdapter.ViewPagerHolder>() {
-    var photoList= listOf<Photo>()
-    @SuppressLint("NotifyDataSetChanged")
-    set(value) {
-        field=value
-        notifyDataSetChanged()
-    }
+class ViewPagerAdapter:PagingDataAdapter<Photo,ViewPagerAdapter.ViewPagerHolder>(PhotosAdapter.DiffUtilCallback()) {
     class ViewPagerHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
         val imageView:ImageView = itemView.findViewById(R.id.imagePager)
 
@@ -27,12 +22,10 @@ class ViewPagerAdapter:RecyclerView.Adapter<ViewPagerAdapter.ViewPagerHolder>() 
     }
 
     override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
-        val currentImage = photoList[position]
-        Picasso.get().load(currentImage.urls.urlRegular).into(holder.imageView)
+        val currentImage = getItem(position)
+        Picasso.get().load(currentImage?.urls?.urlRegular).into(holder.imageView)
 //        holder.imageView.setImageResource(currentImage)
 
     }
-
-    override fun getItemCount(): Int = photoList.size
 
 }
