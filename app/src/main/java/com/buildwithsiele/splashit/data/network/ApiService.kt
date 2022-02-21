0eeb42466245162f223c9a9f6b77e38a4f1255be
kimsiele <sielekim.emmanuel@gmail.com>
2022-02-21 22:29:28 +0300

    Fixes

--- app/src/main/java/com/buildwithsiele/splashit/data/network/ApiService.kt
+++ app/src/main/java/com/buildwithsiele/splashit/data/network/ApiService.kt
@@ -1,13 +1,14 @@
 package com.buildwithsiele.splashit.data.network
 
-import android.os.Build
-import androidx.annotation.RequiresApi
+import com.buildwithsiele.splashit.BuildConfig
 import com.buildwithsiele.splashit.data.model.Photo
+import com.buildwithsiele.splashit.data.model.SearchResponse
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 
@@ -29,9 +30,17 @@ interface ApiService {
     @GET("photos")
     suspend fun getPhotos(
         @Query("page") page:Int = 1,
-        @Query("per_page") per_page:Int = 21,
-        @Query("client_id") clientId: String = "Y7Eo8c546fG_BeKByFyoYucQhSjAjDkz1kM3YZHVrnE"
+        @Query("per_page") per_page:Int = 18,
+        @Query("client_id") clientId: String = BuildConfig.UNSPLASH_API_KEY
     ): List<Photo>
+
+    @GET("search/photos")
+    suspend fun search(
+        @Query("client_id") clientId: String = BuildConfig.UNSPLASH_API_KEY,
+        @Query("query") query: String,
+        @Query("page") page:Int = 1,
+        @Query("per_page") per_page:Int = 18,
+    ):SearchResponse
 }
 
 object PhotosApi {
